962f514fb9e23177f657eecb1abdca0af76a7462
Abdul Wahid <111135216+abdulwahid4423@users.noreply.github.com>
2022-09-02 07:31:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,186 @@
+// alert  ("Error!plese enter a valid password");
+// alert ("Walcome to js Land...\nHappy coding \n!")
+//  alert ("Welcome to js land...")
+//  alert ("Happy coding \n Prevent this page from creating additional dialoge \n");
+// alert  ("Hello.. I can run throgh my wed brower consols ")
+// chepter2
+// var usernane = "wahid ";
+// var myname = "abdulWahid"
+// var messege ="Hello world"
+// alert(usernane)
+// alert(myname)
+// alert(messege) 
+
+// var a = "plzza\n"
+// var b = "plzz\n"
+// var c = "plz\n"
+// var d = "pl\n"
+// var e = "p\n"
+//  var all= a+b+c+d+e 
+//  alert (all)
+
+// document.write ("Yeh!l can write HTML content throgh javascript")
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// chepter3
+
+// var age = ("lam 15 year old");alert(age)
+// var a = ("You have visited this site 14 time")
+// alert(a)
+// document.write("My brith year is 1999 \nDate type of me decleared variofle is number"); 
+//  chapter4
+// var abc = "legal"
+// var 1abc = "inligal"
+// var asd2 = "legal"
+// var 123 = "inligal"
+// var ab1c = "legal"
+// var % = "inligal"
+// var $ = "legal"
+// var -v = "inligal"
+// var a$c = "legal"
+
+// chapte5
+
+// var a = 3 
+// var b = 5 
+// document.write(a+b)
+
+// var a = 3 
+// var b = 5 
+// document.write(a-b)
+
+// var a = 3 
+// var b = 5 
+// document.write(a*b)
+
+// var a = 3 
+// var b = 5 
+// document.write(a/b)
+
+// var a = 3 
+// var b = 5 
+// document.write(a%b)
+
+// var a = 3 
+// var b = 5 
+// document.write(a++ + ++b)
+
+// chapte6 to 9
+
+// var a = 10
+// var b 
+// alert  (++a - a++ + --a - a++)
+
+// var a = 2
+// var b = 1
+// alert (--a - --b + ++b + b--)
+
+// var city = prompt("enter city name" )
+// if(city == "karachi"){
+//     document.write("welcome to city of light")
+// }
+// else{document.write("welcome to city")} 
+
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// chapte11to12
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// document.write("Good day");
+// }
+// else{
+// document.write("Good evening")
+// }
+
+// chapte 14to16
+
+// var student = ["wahid","ali","raza","iqra","suba","azhar","aliza"]
+// student.push ("sameer")
+// console.log (student)
+
+// var student = ["wahid","ali","raza","iqra","suba","azhar","aliza"]
+// student.pop()
+// console.log (student)
+
+// var student = ["wahid","ali","raza","iqra","suba","azhar","aliza"]
+// student.splice (0,4,"hormat")
+//  console.log (student)
+
+// var student = ["wahid","ali","raza","iqra","suba","azhar","aliza"]
+// student.slice (0,4,"hormat")
+//  console.log (student)
+
+// var number =["89","23","45","87","12"]
+// console.log(number) 
+
+// var buleen = ["true","false"]
+// console.log (buleen)
+
+
+//chapter 20
+
+
+
+// var a;
+// for (a=1;a<=10 ; a++ ){
+// console.log (a)}
+
+// var a;
+// for (a=10;a>=1 ; a-- ){
+// console.log (a)}
+
+// var a;
+// for (a=1;a<=10; a++){
+// document.write ("8" + "x" + a + "="  + 8*a + "<br>")
+// }
+ 
+// var cities = ["karachi","islambad","lahor"]
+// for( var i = 0 ; i <cities.length; i++){
+// if(cities[i]==="karachi")
+// alert(cities[i]+ "welcome")
+// }
+
+// var fistName= [ "iqra","sabe","aliza"]
+// var lastName= ["wahid"]
+
+// for (var i=0; i < fistName.length; i++){
+//  for ( x=0; x< lastName.length;x++ ){
+//    console.log (fistName[i] + "" + lastName[x] )
+//  }
+// }
+
+
+
+
+
+
+
